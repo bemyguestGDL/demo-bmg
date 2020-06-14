930f0ba792c1a3d53f21d5d367ab1ac60b468b63
Rene Barrera <rene.barrera1022@gmail.com>
2020-06-13 21:01:08 -0500

    hbjnklñ

--- sw.js
+++ sw.js
@@ -52,7 +52,6 @@ const appShell = [
 ];
 
 const appShellInmutable = [
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119254.90084806681!2d-100.83375925900282!3d20.923756694664274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd047b2f9edd984b!2sRosewood%20San%20Miguel%20de%20Allende!5e0!3m2!1ses-419!2smx!4v1592080924197!5m2!1ses-419!2smx',
     'https://kit.fontawesome.com/3e7a1a3c47.js',
     'https://fonts.googleapis.com/css2?family=Bentham&family=Josefin+Sans:wght@300&family=Poiret+One&family=Quicksand&family=Tenor+Sans&display=swap',
     'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
